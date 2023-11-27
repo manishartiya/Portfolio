8117650b0a446398c1e7df731e636d9b6ee501ca
manishartiya <manishkumaraartiya93@gmail.com>
2023-11-27 23:19:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+
+  $('.portfolio .button-container .btn').click(function(){
+
+    let filter = $(this).attr('data-filter');
+
+    if(filter == 'all'){
+      $('.portfolio .image-container .box').show('400')
+    }
+  });
+
+  $('#theme-toggler').click(function(){
+    $(this).toggleClass('fa-sun');
+    $('body').toggleClass('dark-theme');
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500,
+      'linear'
+    );
+
+  });
+
+});
+
+window.addEventListener('scroll',reveal);
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+  for (var i=0;i<reveals.length;i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add("active");
+    }
+    // else{
+    //   reveals[i].classList.remove("active");
+    // }
+  } 
+}
